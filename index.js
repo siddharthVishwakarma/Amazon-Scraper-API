38d4809db25305dbe1e8f3b685896839d6594e62
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-03-19 13:03:18 +0530

    endpoint 1 -> get product details

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const apiKey = "08c76fdd1f07e2ae7cb92e7033b508fa";
+
+const baseURL = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Amazon Scraper API");
+});
+
+// Ger Product details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const response = await request(
+      `${baseURL}&url=https://www.amazon.in/dp/${productId}`
+    );
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
